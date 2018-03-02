c98f4ca4fee1b5356e2baa53d3ab40c61e5c6e23
Amylouise94 <amy.mccormack@ucdconnect.ie>
2018-03-02 12:56:08 +0000

    Adding in RegEx and Parse functions/tests - Coord bug fixed

--- LED_Display/main.py
+++ LED_Display/main.py
@@ -5,6 +5,40 @@ Created on 1 Mar 2018
 '''
 
 import numpy as np
+import requests
+import re
+import os
+
+def parseFile(input):
+    if input.startswith('http'):
+        req = requests.get(input).text
+        if req is not None:
+            return True
+        else:
+            return False
+        
+    else:    
+        if not os.path.isfile(input):
+            return False
+            #print(input,"does not exist")
+        else:
+            file = open(input, 'r')
+            if file is not None:
+                for line in file.readlines():
+                    return line
+                
+            file.close()
+parseFile('testfile.txt')
+parseFile("http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt")
+      
+def regEx(line):
+    pat = re.compile(".*(turn on|turn off|switch)\s*([+-]?\d+)\s*,\s*([+-]?\d+)\s*through\s*([+-]?\d+)\s*,\s*([+-]?\d+).*")
+    m = pat.match(line)     
+    if m is not None:
+        return True
+    else:
+        return False
+    
 
 class ledDisplay():
     '''
@@ -27,22 +61,22 @@ class ledDisplay():
         self.count = np.sum(self.lights)
         return self.count
     
-    def switch(self, startRow, startColumn, stopColumn, stopRow):
+    def switch(self, startRow, startColumn, stopRow, stopColumn):
         
-        for i in range(startRow, stopRow):
-            for j in range(startColumn, stopColumn):
+        for i in range(startRow, stopRow+1):
+            for j in range(startColumn, stopColumn+1):
                 if self.lights[i][j] == False:
                     self.lights[i][j] = True
                 else:
                     self.lights[i][j] = False
     
-    def turnOn(self, startRow, startColumn, stopColumn, stopRow):
-        for i in range(startRow, stopRow):
-            for j in range(startColumn, stopColumn):
+    def turnOn(self, startRow, startColumn, stopRow, stopColumn):
+        for i in range(startRow, stopRow+1):
+            for j in range(startColumn, stopColumn+1):
                 self.lights[i][j] = True
     
-    def turnOff(self, startRow, startColumn, stopColumn, stopRow):
-        for i in range(startRow, stopRow):
-            for j in range(startColumn, stopColumn):
+    def turnOff(self, startRow, startColumn, stopRow, stopColumn):
+        for i in range(startRow, stopRow+1):
+            for j in range(startColumn, stopColumn+1):
                 self.lights[i][j] = False
     
--- Tests/LED_Test.py
+++ Tests/LED_Test.py
@@ -11,27 +11,44 @@ def testGrid():
     big = ledDisplay(2)
     assert big.gridSize() == 2
     assert big.count() == 0
-    #print(big.gridSize()) 
-    #print(big.grid())
-    #print(big.count())
 
 def testSwitch():
     grid = ledDisplay(5)
     grid.switch(1, 1, 3, 3)
-    #assert grid.count() == 4
-    #print(grid.grid())
     grid.switch(1, 2, 4, 4)
-    #print(grid.grid())
-    #print(grid.count())
-    assert grid.count() == 6
+    assert grid.count() == 9
 
 def testTurnOn():
     grid = ledDisplay(5)
-    grid.turnOn(2, 2, 5, 5)
-    grid.turnOn(3, 4 , 5, 3)
+    grid.turnOn(2, 2, 4, 4)
+    grid.turnOn(3, 4 , 4, 3)
     grid.turnOn(1, 1, 2, 2)
-    assert grid.count() == 13
+    assert grid.count() == 12
+
+def testTurnOff():
+    grid = ledDisplay(6)
+    grid.turnOn(0, 0, 5, 5)
+    grid.turnOff(0, 2, 5, 4)
+    grid.turnOff(1, 2, 5, 3)
+    grid.turnOff(0, 1, 0, 4)
+    assert grid.count() == 17
+
+def testRegEx():
+    string = "switcy 166,675 through 329,961"
+    assert regEx(string) == False
+    string2 = "switch 166,675 through 329,961"
+    assert regEx(string2) == True
+    
+def testParse():
+    assert parseFile('ttestfile.txt') == False
+    assert parseFile("http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt") == True
+    assert parseFile('testfile.txt') == 'This is a testfile'
+
 
 if __name__ == '__main__':
     testGrid()
     testSwitch()
+    testTurnOn()
+    testTurnOff()
+    testRegEx()
+    testParse()
